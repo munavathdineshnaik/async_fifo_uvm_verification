b7424a1ac41a52a663bb84f02062aaee7d2d9ad9
munavathdineshnaik <munavathdineshnaik999@gmail.com>
2026-01-22 18:23:20 +0530

    Update fifo_transaction.sv

--- tb/fifo_transaction.sv
+++ tb/fifo_transaction.sv
@@ -5,36 +5,27 @@ class fifo_transaction extends uvm_sequence_item;
     rand bit read_en;
 
     // Data associated with write operations
-    rand bit [7:0] write_data;
+    rand bit [DATA_WIDTH-1:0] write_data;
 
-    // Observed read data 
-    bit  [7:0] read_data;
+    // Observed read data
+    bit  [DATA_WIDTH-1:0] read_data;
 
     // Constraints
-
-    // Do not allow read and write to be asserted together
-    constraint rw_exclusive {
-        !(write_en && read_en);
-    }
-
-    // If read is enabled, write must be disabled
+    // Allow either read or write, but not both
     constraint valid_ops {
-        write_en != read_en;
+        write_en ^ read_en;
     }
 
     // UVM automation
     `uvm_object_utils_begin(fifo_transaction)
-        `uvm_field_int(write_en , UVM_DEFAULT)
-        `uvm_field_int(read_en  , UVM_DEFAULT)
+        `uvm_field_int(write_en  , UVM_DEFAULT)
+        `uvm_field_int(read_en   , UVM_DEFAULT)
         `uvm_field_int(write_data, UVM_DEFAULT)
         `uvm_field_int(read_data , UVM_DEFAULT | UVM_NOPACK)
     `uvm_object_utils_end
-
-    // Constructor
 
     function new(string name = "fifo_transaction");
         super.new(name);
     endfunction
 
 endclass
-
